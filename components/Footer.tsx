caa86b1844bea8d32d8befad08addba9355a5e0e
egorlagunovich <egorlagunovich@gmail.com>
2023-09-05 12:08:36 +0200

    footer skeleton

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='h-1/2 bg-black text-white snap-start'>Footer</footer>
+    <footer className='h-1/2 bg-black text-white snap-start'>
+      <span>© Copyright 2023</span>-<a href='#home'>Egor Lagunovich</a>
+    </footer> 
   )
 }
 
