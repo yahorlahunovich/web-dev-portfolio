6f03e757d7ea38c9b71a0f2c5ed918ea2f5c1fd0
egorlagunovich <egorlagunovich@gmail.com>
2023-09-29 10:08:32 +0200

    mockup of contact section

--- components/layout/Contact.tsx
+++ components/layout/Contact.tsx
@@ -9,31 +9,37 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="flex flex-col justify-center min-h-screen text-3xl snap-start px-32 main-bg "
+      className="w-full flex flex-col sm:flex-row gap-5 justify-center items-center min-h-screen text-3xl snap-start sm:px-32 main-bg"
       data-aos="fade-up"
     >
-      <h1 className="text-center">Contact</h1>
-      <div className="flex flex-row justify-center items-center ">
-        <div className="w-1/2">
-          <Image src={contactImg} alt="Mountain" />
+      <div className="w-full sm:w-1/2">
+        <Image src={contactImg} alt="Mountain" />
+      </div>
+      <div className="flex flex-col flex-wrap justify-center items-center w-full sm:w-1/2">
+        <h2 className="text-center m-2">Get in Touch</h2>
+        <p>
+          Have a project in mind or simply want to connect? I'm here to make it
+          easy. The Contact section is your direct line to me. Whether you have
+          questions, ideas, or opportunities to discuss, I'm all ears.
+        </p>
+        <p>I'm also open to full-time hiring.</p>
+        <div className="flex flex-row gap-2 justify-center items-center">
+          My email is{" "}
+          <a
+            href="mailto: egorlagunovich@gmail.com"
+            className="flex flex-row justify-center items-center"
+          >
+            <Image src={envelopeSvg} alt="envelope icon" width={20} />{" "}
+            egorlagunovich@gmail.com
+          </a>
         </div>
-        <div className="flex flex-col justify-center items-center w-1/2">
-          <h2>Do you want to work with me?</h2>
-          <p>Let's get in touch!</p>
-          <div className="flex">
-            My email is{" "}
-            <a href="mailto: egorlagunovich@gmail.com" className="text-red-600">
-              <Image src={envelopeSvg} alt="envelope icon" width={20} />{" "}
-              egorlagunovich@gmail.com
-            </a>
-          </div>
-          <div>
-            {social_media_links.map((link) => (
-              <Link href={link.src} key={link.name}>
-                <Image src={link.iconSrc} alt={link.name} />
-              </Link>
-            ))}
-          </div>
+        <h3>Social media</h3>
+        <div className="flex flex-row justify-center items-center">
+          {social_media_links.map((link) => (
+            <Link href={link.src} key={link.name}>
+              <Image src={link.iconSrc} alt={link.name} width={150} />
+            </Link>
+          ))}
         </div>
       </div>
     </section>
